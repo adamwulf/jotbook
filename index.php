582e7c0f3c54aaa09be6990f5bfa5e0534fa1a8c
Adam Wulf <adam.wulf@gmail.com>
2014-01-11 18:36:15 -0500

    moved much of the logic into a class to clean up the index file quite a bit.

--- index.php
+++ index.php
@@ -7,103 +7,53 @@ include "config.php";
 require_once ('codebird-php/src/codebird.php');
 $classLoader->addToClasspath(ROOT . "json-to-mysql/");
 
+// start session for tracking logged in user
+session_start();
+
+// create global objects
+
 $mysql = new MySQLConn(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASS);
 $db = new JSONtoMYSQL($mysql);
 \Codebird\Codebird::setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
 $cb = \Codebird\Codebird::getInstance();
 
-
-// start session for tracking logged in user
-session_start();
-
-// init a session id if we don't already have one
-if(!isset($_SESSION["uuid"])){
-	// all our access will be through this uuid
-	$_SESSION["uuid"] = gen_uuid();
-}
+$app = new EasyApp($db, $cb);
 
 
 
-$sessions = $db->table("sessions");
-$session_info = $sessions->find(array("session_id" => $_SESSION["uuid"]));
+
+
+
 
 if(isset($_GET["logout"])){
-	$sessions = $db->table("sessions");
-	$session_info = $sessions->delete(array("session_id" => $_SESSION["uuid"]));
-	session_unset();
+	$app->logout();
     header('Location: ' . page_self_url());
     die();
 }else if(isset($_GET['twitter_login'])){
 	if (!isset($_SESSION['twitter_oauth_verify'])) {
-	    // get the request token
-	    $reply = $cb->oauth_requestToken(array(
+		$auth_url = $app->twitterLogin(array(
 	        'oauth_callback' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
 	        'state' => 'twitter_login'
 	    ));
-	
-	    // store the token
-	    $cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
-	    $_SESSION['twitter_oauth_token'] = $reply->oauth_token;
-	    $_SESSION['twitter_oauth_token_secret'] = $reply->oauth_token_secret;
-	    $_SESSION['twitter_oauth_verify'] = true;
-	
-	    // redirect to auth website
-	    $auth_url = $cb->oauth_authorize();
 	    header('Location: ' . $auth_url);
 	    die();
-	
 	}elseif (isset($_GET['oauth_verifier']) && isset($_SESSION['twitter_oauth_verify'])) {
 	
-	
-	    // verify the token
-	    $cb->setToken($_SESSION['twitter_oauth_token'], $_SESSION['twitter_oauth_token_secret']);
-	    unset($_SESSION['twitter_oauth_verify']);
-	
-	    // get the access token
-	    $reply = $cb->oauth_accessToken(array(
-	        'oauth_verifier' => $_GET['oauth_verifier']
-	    ));
-	
-		unset($reply->httpstatus);
-		$reply->id = $reply->user_id;
-		$ret = $db->save($reply, "twitter_login");
-		
-		
-		$sess = array("session_id" => $_SESSION["uuid"],
-					  "twitter_id" => $reply->id);
-		$ret = $db->save($sess, "sessions");
-		
-		if($ret == JSONtoMYSQL::$UPDATE){
-			$done = true;
-		}
+		$app->verifyLogin($_GET['oauth_verifier']);
 	
 	    // send to same URL, without oauth GET parameters
 	    header('Location: ' . page_self_url());
 	    die();
 	}
 }
-// don't allow any session oauth vars outside of here...
-unset($_SESSION['twitter_oauth_token']);
-unset($_SESSION['twitter_oauth_token_secret']);
-unset($_SESSION['twitter_oauth_verify']);
 
-if($session_info->num_rows()){
+if($app->isLoggedIn()){
 	echo "logged in<br>";
 	echo "<a href='" . page_self_url() . "?logout" . "'>Log Out</a><br>";
 	
 	echo "<br><br>";
 	
-	$session_info = $session_info->fetch_array();
-	$twitter_info = $db->table("twitter_login")->find(array("id" => $session_info["twitter_id"]))->fetch_array();
-	
-	print_r($twitter_info);
-
-	// assign access token on each page load
-	$cb->setToken($twitter_info['oauth_token'], $twitter_info['oauth_token_secret']);
-	
-	$reply = $cb->account_verifyCredentials();
-	print_r($reply);
-
+	print_r($app->fetchSomeTwitterInfo());
 }else{
 	echo "<a href='" . page_self_url() . "?twitter_login" . "'>";
 	echo "<img src='" . page_self_url() . "images/sign-in-with-twitter-gray.png' border=0/>";
---
+++ library/EasyApp/class.EasyApp.php
@@ -0,0 +1,116 @@
+<?
+
+/**
+ * The barebones application, using twitter oauth for
+ * user management
+ */
+class EasyApp{
+
+	// json to mysql
+	private $db;
+	
+	// codebird twitter
+	private $cb;
+	
+	// session info
+	private $session_info;
+
+	function __construct($db, $cb){
+		$this->db = $db;
+		$this->cb = $cb;
+		$this->initSession();
+	}
+	
+	private function initSession(){
+		// init a session id if we don't already have one
+		if(!isset($_SESSION["uuid"])){
+			// all our access will be through this uuid
+			$_SESSION["uuid"] = gen_uuid();
+		}
+	}
+	
+	private function sessionId(){
+		// init session if needed
+		$this->initSession();
+		// return id
+		return $_SESSION["uuid"];
+	}
+	
+	private function sessionInfo(){
+		if(!$this->session_info){
+			$sessions = $this->db->table("sessions");
+			$this->session_info = $sessions->find(array("session_id" => $this->sessionId()))->fetch_array();
+		}
+		return $this->session_info;
+	}
+	
+	
+	public function isLoggedIn(){
+		return is_array($this->sessionInfo());
+	}
+	
+	
+	
+	public function logout(){
+		$sessions = $this->db->table("sessions");
+		$sessions->delete(array("session_id" => $_SESSION["uuid"]));
+		$this->session_info = false;
+		session_unset();
+	}
+
+	public function twitterLogin($callback_params){
+	    // get the request token
+	    $reply = $this->cb->oauth_requestToken($callback_params);
+	    // store the token
+	    $this->cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
+	    $_SESSION['twitter_oauth_token'] = $reply->oauth_token;
+	    $_SESSION['twitter_oauth_token_secret'] = $reply->oauth_token_secret;
+	    $_SESSION['twitter_oauth_verify'] = true;
+	    // redirect to auth website
+	    return $this->cb->oauth_authorize();
+	}
+	
+	public function verifyLogin($oauth_verifier){
+	    // verify the token
+	    $this->cb->setToken($_SESSION['twitter_oauth_token'], $_SESSION['twitter_oauth_token_secret']);
+	    unset($_SESSION['twitter_oauth_verify']);
+	
+	    // get the access token
+	    $reply = $this->cb->oauth_accessToken(array(
+	        'oauth_verifier' => $oauth_verifier
+	    ));
+	
+		unset($reply->httpstatus);
+		$reply->id = $reply->user_id;
+		$ret = $this->db->save($reply, "twitter_login");
+		
+		
+		$sess = array("session_id" => $_SESSION["uuid"],
+					  "twitter_id" => $reply->id);
+		$ret = $this->db->save($sess, "sessions");
+		
+		if($ret == JSONtoMYSQL::$UPDATE){
+			$done = true;
+		}
+		// don't allow any session oauth vars outside of here...
+		unset($_SESSION['twitter_oauth_token']);
+		unset($_SESSION['twitter_oauth_token_secret']);
+		unset($_SESSION['twitter_oauth_verify']);
+	}
+	
+	
+	public function fetchSomeTwitterInfo(){
+		$session_info = $this->sessionInfo();
+		$twitter_info = $this->db->table("twitter_login")->find(array("id" => $session_info["twitter_id"]))->fetch_array();
+		
+		print_r($twitter_info);
+	
+		// assign access token on each page load
+		$this->cb->setToken($twitter_info['oauth_token'], $twitter_info['oauth_token_secret']);
+		
+		$reply = $this->cb->account_verifyCredentials();
+		return $reply;
+	}
+}
+
+?>
