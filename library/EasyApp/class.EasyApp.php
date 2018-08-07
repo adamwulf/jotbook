9e3c1ead962e174f5c5bf2860bda8920bca78e30
Adam Wulf <adam.wulf@gmail.com>
2018-08-07 01:11:08 -0400

    Removed sessions table, session uuid

--- library/EasyApp/class.EasyApp.php
+++ library/EasyApp/class.EasyApp.php
@@ -12,61 +12,39 @@ class EasyApp{
 	// codebird twitter
 	private $cb;
 	
-	// session info
-	private $session_info;
-	
 	// the twitter app object if logged in
 	private $twitter;
 
 	function __construct($db){
+		
 		$this->db = $db;
 		$this->cb = new \Codebird\Codebird;
-		$this->initSession();
-	}
-	
-	private function initSession(){
-		// init a session id if we don't already have one
-		if(!isset($_SESSION["uuid"])){
-			// all our access will be through this uuid
-			$_SESSION["uuid"] = gen_uuid();
-		}
-	}
-	
-	private function sessionId(){
-		// init session if needed
-		$this->initSession();
-		// return id
-		return $_SESSION["uuid"];
-	}
-	
-	private function sessionInfo(){
-		if(!$this->session_info){
-			$sessions = $this->db->table("sessions");
-			$this->session_info = $sessions->find(array("session_id" => $this->sessionId()))->fetch_array();
-		}
-		return $this->session_info;
 	}
 	
 	public function isLoggedIn(){
-		if(!$this->twitter && is_array($this->sessionInfo())){
-			$session_info = $this->sessionInfo();
-			$twitter_info = $this->db->table("twitter_login")->find(array("id" => $session_info["twitter_id"]))->fetch_array();
+		if(!$this->twitter && isset($_SESSION["twitter_id"])){
+			$twitter_info = $this->db->table("twitter_login")->find(array("id" => $_SESSION["twitter_id"]))->fetch_array();
 			$this->twitter = new EasyAppTwitter($twitter_info);
 		}
 		return is_object($this->twitter);
 	}
 	
 	public function logout(){
-		$sessions = $this->db->table("sessions");
-		$sessions->delete(array("session_id" => $_SESSION["uuid"]));
-		$this->session_info = false;
 		$this->twitter = false;
-		session_unset();
-	}
+	    session_unset();
+	    session_destroy();
+	    session_write_close();
+	    setcookie(session_name(),'',0,'/');
+	    session_regenerate_id(true);
+   	}
 	
 	public function twitter(){
-		$this->isLoggedIn(); // check login and setup twitter if need-be
-		return $this->twitter;
+		if($this->isLoggedIn()){
+			return $this->twitter;
+		}else{
+			throw new Exception();
+			return null;
+		}
 	}
 
 	public function twitterLogin($callback_params){
@@ -96,9 +74,7 @@ class EasyApp{
 		$reply->id = $reply->user_id;
 		$ret = $this->db->save($reply, "twitter_login");
 		
-		$sess = array("session_id" => $_SESSION["uuid"],
-					  "twitter_id" => $reply->id);
-		$ret = $this->db->save($sess, "sessions");
+		$_SESSION["twitter_id"] = $reply->id;
 		
 		if($ret == JSONtoMYSQL::$UPDATE){
 			$done = true;
@@ -111,7 +87,6 @@ class EasyApp{
 		unset($_SESSION['twitter_oauth_token_secret']);
 		unset($_SESSION['twitter_oauth_verify']);
 		unset($_SESSION['true_callback']);
-		
 		
 		$profile = $this->twitter()->fetchSomeTwitterInfo();
 		
